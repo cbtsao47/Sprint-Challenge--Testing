f9e8acd651cdca1bc1e673c1a4084e37d662af77
wisdomtooth <cbtsao47@gmail.com>
2019-02-01 10:36:14 -0600

    add schema table

---
+++ api/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const configMiddleware = require("../config/middleware");
+const server = express();
+const db = require("../data/dbConfig");
+configMiddleware(server);
+
+module.exports = server;
